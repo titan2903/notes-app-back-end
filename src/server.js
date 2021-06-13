65cf83ecb6f16735ea36cf3a4d31cd8ae6c81bca
Titanio Yudista <titanioyudista98@gmail.com>
2021-06-13 08:38:07 +0700

    update API notes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,32 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+require('dotenv').config()
 
 const init = async () => {
+  const notesService = new NotesService();
+ 
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
       },
     },
   });
-
-  server.route(routes);
-
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+ 
   await server.start();
-  // eslint-disable-next-line no-console
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
+ 
 init();
